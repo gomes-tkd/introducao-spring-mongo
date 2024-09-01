6c28b265157e68d98eb08ae0874e82c97dfd0a6e
gomes-tkd <jgomestkd@gmail.com>
2024-09-01 19:08:32 -0300

    Entity User e REST funcionando

---
+++ src/main/java/io/github/gomestkd/introducaospringmongo/IntroducaoSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package io.github.gomestkd.introducaospringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoSpringMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroducaoSpringMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/gomestkd/introducaospringmongo/IntroducaoSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gomestkd.introducaospringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroducaoSpringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

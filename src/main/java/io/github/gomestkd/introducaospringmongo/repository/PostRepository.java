08e7dc87a21bee90e8e50fb07e1239cba27b02c1
gomes-tkd <jgomestkd@gmail.com>
2024-09-02 18:47:07 -0300

    Consulta simples com @Query

--- src/main/java/io/github/gomestkd/introducaospringmongo/repository/PostRepository.java
+++ src/main/java/io/github/gomestkd/introducaospringmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package io.github.gomestkd.introducaospringmongo.repository;
 
 import io.github.gomestkd.introducaospringmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String title);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' }}")
+    List<Post> searchTitle(String text);
 }
